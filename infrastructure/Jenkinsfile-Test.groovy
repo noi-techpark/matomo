52e741500db3e1933b4ed18b1e21cc8f176592a0
Simon Dalvai <s.dalvai@noi.bz.it>
2022-02-08 09:53:55 +0100

    escape backslash

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -37,7 +37,7 @@ pipeline {
           echo '    ## only allow accessing the following php files' >> matomo.conf
           echo '    location ~ ^/(index|matomo|piwik|js/index|plugins/HeatmapSessionRecording/configs).php {' >> matomo.conf
           echo '      # regex to split $uri to $fastcgi_script_name and $fastcgi_path' >> matomo.conf
-          echo '      fastcgi_split_path_info ^(.+\.php)(/.+)$;' >> matomo.conf
+          echo '      fastcgi_split_path_info ^(.+\\.php)(/.+)$;' >> matomo.conf
           echo '      # Check that the PHP script exists before passing it' >> matomo.conf
           echo '      try_files $fastcgi_script_name =404;' >> matomo.conf
           echo '      include fastcgi_params;' >> matomo.conf
@@ -56,15 +56,15 @@ pipeline {
           echo '      deny all;' >> matomo.conf
           echo '      return 403; # replace with 404 to not show these directories exist' >> matomo.conf
           echo '    }' >> matomo.conf
-          echo '    location ~ /\.ht {' >> matomo.conf
+          echo '    location ~ /\\.ht {' >> matomo.conf
           echo '      deny all;' >> matomo.conf
           echo '      return 403;' >> matomo.conf
           echo '    }' >> matomo.conf
-          echo '    location ~ js/container_.*_preview\.js$ {' >> matomo.conf
+          echo '    location ~ js/container_.*_preview\\.js$ {' >> matomo.conf
           echo '      expires off;' >> matomo.conf
           echo '      add_header Cache-Control 'private, no-cache, no-store';' >> matomo.conf
           echo '    }' >> matomo.conf
-          echo '    location ~ \.(gif|ico|jpg|png|svg|js|css|htm|html|mp3|mp4|wav|ogg|avi|ttf|eot|woff|woff2|json)$ {' >> matomo.conf
+          echo '    location ~ \\.(gif|ico|jpg|png|svg|js|css|htm|html|mp3|mp4|wav|ogg|avi|ttf|eot|woff|woff2|json)$ {' >> matomo.conf
           echo '      allow all;' >> matomo.conf
           echo '      ## Cache images,CSS,JS and webfonts for an hour' >> matomo.conf
           echo '      ## Increasing the duration may improve the load-time, but may cause old files to show after an Matomo upgrade' >> matomo.conf
@@ -77,7 +77,7 @@ pipeline {
           echo '      return 403;' >> matomo.conf
           echo '    }' >> matomo.conf
           echo '   ## properly display textfiles in root directory' >> matomo.conf
-          echo '   location ~/(.*\.md|LEGALNOTICE|LICENSE) {' >> matomo.conf
+          echo '   location ~/(.*\\.md|LEGALNOTICE|LICENSE) {' >> matomo.conf
           echo '      default_type text/plain;' >> matomo.conf
           echo '    }' >> matomo.conf
           echo '  }' >> matomo.conf' >> matomo.conf
